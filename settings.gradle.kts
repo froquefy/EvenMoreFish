45fad26aff993114f03c05997f9149d83d09ed04
froquefy <88888842+froquefy@users.noreply.github.com>
2026-07-09 22:30:06 +0200

    build: apply foojay toolchain resolver so JDK toolchains auto-provision

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,12 @@ rootProject.name = "even-more-fish"
 
 pluginManagement {
     plugins {
-        id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+        id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
     }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention")
 }
 
 // Addons
